f628a7ff1157e33d6e2452149cb1311e11bfe2df
Iek <iek@iek>
2022-05-16 16:09:32 -0700

    update TODO

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/akamensky/argparse v1.3.1 // indirect
-	github.com/hfern/goasp v0.0.0-20141215223504-cf9cbab88c6a // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 
 )
--- viewstalker.go
+++ viewstalker.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,6 +15,8 @@ import (
 )
 
 func makeRequest(address string) string {
+	fmt.Println(Teal("Trying "))
+	fmt.Printf(Teal("%s\n"), address)
 	resp, err := http.Get(address)
 	if err != nil {
 		log.Fatalln(err)
@@ -27,18 +30,19 @@ func makeRequest(address string) string {
 	if strings.Contains(sb, "__VIEWSTATE") {
 		return sb
 	} else {
-		fmt.Println("Moving on")
+		fmt.Println(Yellow("No Viewstate Found, moving on"))
 	}
 	return "0"
 }
 
-func extractViewstate(body string) [2]string {
+func extractViewstate(body string) (string, string) {
 	doc, err := html.Parse(strings.NewReader(body))
 	if err != nil {
 		log.Fatal(err)
 	}
 	var f func(*html.Node)
-	var retValues [2]string
+	var modifer string
+	var viewstate string
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "input" {
 			var values []string
@@ -46,10 +50,10 @@ func extractViewstate(body string) [2]string {
 				values = append(values, a.Val)
 			}
 			if contains(values, "__VIEWSTATE") {
-				retValues[0] = values[len(values)-1]
+				viewstate = values[len(values)-1]
 			}
 			if contains(values, "__VIEWSTATEGENERATOR") {
-				retValues[1] = values[len(values)-1]
+				modifer = values[len(values)-1]
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
@@ -57,7 +61,7 @@ func extractViewstate(body string) [2]string {
 		}
 	}
 	f(doc)
-	return retValues
+	return viewstate, modifer
 }
 
 func contains(array []string, term string) bool {
@@ -68,34 +72,131 @@ func contains(array []string, term string) bool {
 	}
 	return false
 }
+func containsInt(array []int, term int) bool {
+	for _, v := range array {
+		if v == term {
+			return true
+		}
+	}
+	return false
+}
+
+func validate(viewstate []byte) bool {
+	var PREAMBLE = []byte{255, 1}
+	return bytes.Equal(viewstate[:2], PREAMBLE)
+}
+func decode(validationKey string, validationAlgorithm string, protectedData []byte, modifier string) bool {
+	var match bool
+	var err error
+	if validate(protectedData) {
+		match, err = decodeData(validationKey, validationAlgorithm, protectedData, modifier)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return match
+}
+
 func main() {
 
 	parser := argparse.NewParser("viewStalker", "A tool for identifying vulnerable ASP.NET viewstates")
 	hosts := parser.File("l", "hosts", os.O_RDWR, 0600, &argparse.Options{Required: false, Help: "Path to file with list of hosts to check, one per line"})
+	keys := parser.File("M", "mac", os.O_RDWR, 0600, &argparse.Options{Required: false, Help: "machine keys file from blacklist3r"})
 	address := parser.String("a", "address", &argparse.Options{Required: false, Help: "Single host to check"})
+	test := parser.String("t", "test", &argparse.Options{Required: false, Help: "Single host to check"})
+	testViewstate := parser.String("v", "viewstate", &argparse.Options{Required: false, Help: "b64 encoded viewstate"})
+	testModifier := parser.String("m", "modifier", &argparse.Options{Required: false, Help: "modifer"})
+	testValKey := parser.String("k", "key", &argparse.Options{Required: false, Help: "validation key"})
+	testAlgo := parser.String("s", "algo", &argparse.Options{Required: false, Help: "algorithm for validation"})
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
+		log.Fatal(err)
 	}
+
+	keyFile := *keys
+	testflag := *test
 	addressValue := *address
 	hostsFile := *hosts
-	if strings.Contains(addressValue, "http") {
+
+	if strings.Contains(testflag, "viewstate") {
+		viewstate := *testViewstate
+		valKey := *testValKey
+		modifier := *testModifier
+		algo := *testAlgo
+		if len(viewstate) == 0 || len(valKey) == 0 || len(modifier) == 0 || len(algo) == 0 {
+			fmt.Print(parser.Usage(err))
+			fmt.Println(Red("Required: viewstate, modifier, algo, key"))
+			os.Exit(0)
+		}
+		vs, err := base64.StdEncoding.DecodeString(viewstate)
+		if err != nil {
+			log.Fatal(err)
+		}
+		match := decode(valKey, algo, vs, modifier)
+		if match {
+			fmt.Println(Green("KEY FOUND!!!"))
+		} else {
+			fmt.Println(Red("KEY NOT FOUND :("))
+		}
+
+	} else if strings.Contains(addressValue, "http") {
 		sb := makeRequest(addressValue)
 		fmt.Println(extractViewstate(sb))
+
 	} else if !argparse.IsNilFile(&hostsFile) {
-		fmt.Println("Checking for hosts file")
-		file, err := os.Open(hostsFile.Name())
+
+		var vsArray []viewstate
+		keyScanner, keyfile, err := getMachineKeys(keyFile)
 		if err != nil {
 			fmt.Print(parser.Usage(err))
 		}
-		defer file.Close()
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			sb := makeRequest(scanner.Text())
-			fmt.Println(extractViewstate(sb))
+		defer keyfile.Close()
+
+		hostScanner, hostsfile, err := getHosts(hostsFile)
+		if err != nil {
+			fmt.Print(parser.Usage(err))
+		}
+		defer hostsfile.Close()
+
+		for hostScanner.Scan() {
+			address := hostScanner.Text()
+			sb := makeRequest(address)
+			vs, mod := extractViewstate(sb)
+			vstate := viewstate{
+				host:      address,
+				modifier:  mod,
+				viewstate: vs,
+			}
+			vsArray = append(vsArray, vstate)
+
+		}
+		fmt.Printf(Purple("Got: %d viewstate(s)\n"), len(vsArray))
+
+		for _, vs := range vsArray {
+			var validationKey string
+			for keyScanner.Scan() {
+				validationKey = strings.Split(keyScanner.Text(), " ")[0]
+				viewstate, _ := base64.StdEncoding.DecodeString(vs.viewstate)
+				modifier := vs.modifier
+				for _, element := range algorithms {
+					match := decode(validationKey, element, viewstate, modifier)
+					if match {
+						vs.validationKey = validationKey
+						fmt.Println(Green("KEY FOUND!!!"))
+						fmt.Printf("Host: ")
+						fmt.Printf(Yellow(" %s \n"), vs.host)
+						fmt.Printf("Validation Key: ")
+						fmt.Printf(Green("%s"), vs.validationKey)
+					}
+				}
+
+			}
+
 		}
 
-		if err := scanner.Err(); err != nil {
+		if err := hostScanner.Err(); err != nil {
 			log.Fatal(err)
 		}
 	} else {
